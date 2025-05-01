d42ecfdbc6b8d09fdea28005ee9120cb4ed6cbce
晟 尹 <1277820996@qq.com>
2025-05-01 15:59:13 +0800

    里程计数据处理

--- Main_SinsGps.cpp
+++ Main_SinsGps.cpp
@@ -3,7 +3,7 @@
 
 void main(void)
 {
-	psinsdemo(9);
+	psinsdemo(13);
 
 	//CFileRdWt::Dir("D:\\ygm2020\\PSINSÍøÕ¾\\¹ßµ¼Êý¾Ý\\", "D:\\psins210207\\VC60\\Data\\");
 	//CFileRdWt fins("ins.bin"), fkf("kf.bin");
